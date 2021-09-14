543dff99e75422e682a1a82ce9892a4650a6b02f
Vinh Tran <vinhmone@gmail.com>
2021-09-14 09:59:09 +0700

    Init MVVM architecture

---
+++ app/src/main/java/com/sun/quickquotes/base/BaseAdapter.kt
@@ -0,0 +1,18 @@
+package com.sun.quickquotes.base
+
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+
+abstract class BaseAdapter<D, VH : BaseViewHolder<D>>(
+    private val items: MutableList<D>,
+    diffUtil: DiffUtil.ItemCallback<D>
+) : ListAdapter<D, VH>(diffUtil), UpdateData<D> {
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.bindData(getItem(position))
+    }
+
+    override fun updateData(data: List<D>) {
+        submitList(data)
+    }
+}
---
+++ app/src/main/java/com/sun/quickquotes/base/BaseViewModel.kt
@@ -0,0 +1,35 @@
+package com.sun.quickquotes.base
+
+import androidx.annotation.StringRes
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sun.quickquotes.R
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel : ViewModel() {
+
+    private val _isLoading = MutableLiveData<Event<Boolean>>().apply { value = Event(false) }
+    val isLoading: LiveData<Event<Boolean>>
+        get() = _isLoading
+    private val _message = MutableLiveData<Event<@StringRes Int>>()
+    val message: MutableLiveData<Event<Int>>
+        get() = _message
+
+    protected val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        viewModelScope.launch {
+            _message.value = Event(R.string.error_something_wrong)
+            throwable.printStackTrace()
+        }
+    }
+
+    fun showLoading() {
+        _isLoading.value = Event(true)
+    }
+
+    fun hideLoading() {
+        _isLoading.value = Event(false)
+    }
+}
